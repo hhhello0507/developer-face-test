d0aa9878a75c22a8481e042f538fb6dfcaf6977f
hhhello <hhhello0507@dgsw.hs.kr>
2024-12-30 13:41:05 +0900

    mvp완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import S from "./page.module.css";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useRef, useState} from "react";
 import axios from "axios";
 import UploadResponse from "@/app/upload.response";
 import AiResponse from "@/app/ai.response";
 
 export default function Home() {
     const [result, setResult] = useState<{
-        image: string | ArrayBuffer | null,
+        image?: string | ArrayBuffer,
         file: File
     }>();
+    const [aiResponse, setAiResponse] = useState<AiResponse>();
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [isFetching, setIsFetching] = useState(false);
 
     const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
@@ -23,16 +26,17 @@ export default function Home() {
         reader.readAsDataURL(file);
         reader.onload = () => {
             setResult({
-                image: reader.result,
+                image: reader.result ?? undefined,
                 file
             });
         };
     };
 
     const fetchDeveloperType = async () => {
+        setIsFetching(true);
         const file = result?.file;
         if (!file) return;
-        
+
         try {
             const {data}: { data: UploadResponse } = await axios.postForm('/api/upload', {
                 file,
@@ -42,28 +46,80 @@ export default function Home() {
             const {data: aiData}: { data: AiResponse } = await axios.post('/api/ai', {
                 url: data.s3Url
             }, undefined);
-            alert(aiData);
+            setAiResponse(aiData);
         } catch (error) {
             console.log(error);
+        } finally {
+            setIsFetching(false);
         }
-    }
+    };
 
     return (
         <div className={S.page}>
-            <span className={S.title}>너는 딱 OOO 개발자</span>
-            <label className={S.chooseImageLabel} htmlFor="choose-image">이미지 선택</label>
-            <input className={S.chooseImageInput} accept={'image/*'} onChange={handleImageChange} id="choose-image"
-                   type="file"/>
-            {result && (
-                <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                }}>
-                    <img width={360} src={result.image?.toString()} alt=""/>
-                    <button onClick={fetchDeveloperType}>ㄱㄱ</button>
-                </div>
+            {aiResponse ? (
+                <>
+                    <div className={S.title}><span style={{ fontWeight: 400 }}>너는 딱...</span> {aiResponse.developerType}</div>
+                    <span className={S.aiResponseDescription}>{aiResponse.description}</span>
+                    <div className={S.aiResponseSkillContainer}>
+                        {aiResponse.skills.map((skill, index) => (
+                            <span key={index} className={S.aiResponseSkill}>{skill}</span>
+                        ))}
+                    </div>
+                    <button className={S.button} onClick={() => {
+                        setResult(undefined);
+                        setAiResponse(undefined);
+                    }}>다시하기
+                    </button>
+                </>
+            ) : (
+                <>
+                    <span className={S.title}>너는 딱 OOO 개발자</span>
+                    <label className={S.chooseImageLabel} htmlFor="choose-image">
+                        {result?.image === undefined || result.image === null
+                            ? <>이미지 선택</>
+                            : <SelectImage selectedImage={result.image} dismiss={() => {
+                                setResult({
+                                    ...result,
+                                    image: undefined
+                                });
+                                if (inputRef.current) {
+                                    inputRef.current.value = '';
+                                }
+                            }}/>
+                        }
+                    </label>
+                    <input
+                        ref={inputRef}
+                        className={S.chooseImageInput}
+                        onChange={handleImageChange}
+                        accept={'image/*'} id="choose-image"
+                        type="file"/>
+                    <button className={S.button} onClick={fetchDeveloperType} disabled={isFetching || !result}>시작
+                    </button>
+                </>
             )}
+        </div>
+    )
+        ;
+}
+
+function SelectImage(
+    {
+        selectedImage,
+        dismiss
+    }: {
+        selectedImage: string | ArrayBuffer,
+        dismiss: () => void
+    }
+) {
+    return (
+        <div style={{position: 'relative'}}>
+            <button onClick={event => {
+                event.preventDefault();
+                dismiss();
+            }} className={S.chooseImageDismissButton}>X
+            </button>
+            <img className={S.chooseImage} src={selectedImage.toString()} alt=""/>
         </div>
     );
 }
